7a3caeabcefbdf940f3823af33f9474741bacc0e
adamehlers <adamehlers15@gmail.com>
2023-05-17 17:43:21 -0400

    Added the C++ file

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Adam Ehlers
+ *        Date: 05/17/2023
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
